5341f9c6440ea087c7c083f98433cab1f4c1e03f
William Yuan <williamyuan2016@gmail.com>
2026-02-11 15:46:12 -0500

    Implemented basic ray-sphere interaction

--- main.cpp
+++ main.cpp
@@ -4,22 +4,38 @@
 
 #include <iostream>
 
-double mx=-100000, mn=10000000;
+bool hit_sphere(const point3& center, double rad, const ray& r) {
+    // if the discriminant of the formula is negative, no intersection, return false
+    // otherwise return true
+    double a = r.direction().length_squared();
+    double b = dot(-2*r.direction(), center-r.origin());
+    double c = dot(center-r.origin(), center-r.origin()) - rad*rad;
 
-color ray_color(const ray& r) {
-    vec3 unit_direction = unit_vector(r.direction());
-    mx = std::max(mx, unit_direction.y());
-    mn = std::min(mn, unit_direction.y());
-    auto a = 0.5*(unit_direction.y()+1.0);
-    return (1.0-a)*color(1, 1, 1) + a*color(0, 0, 0);
+    return b*b >= 4*a*c;
 }
 
-// Output a simple PPM image
+color ray_color(const ray& r) {
+    if (hit_sphere(point3(-1, -1, -2), 0.5, r)) {
+        return color(1, 0, 0);
+    }
+
+    if (hit_sphere(point3(1, 1, -1), 0.3, r)) {
+        return color(0, 1, 0);
+    }
+
+    if (hit_sphere(point3(-1, 2, -5), 1, r)) {
+        return color(0, 0, 1);
+    }
+    
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y()+1.0);
+    return (1.0-a)*color(1, 1, 1) + a*color(0.071, 0.89, 0.922);
+}
 
 int main() {
     // Aspect ratio and image dimensions
     auto aspect_ratio = 16.0/9.0;
-    int image_width = 400;
+    int image_width = 800;
 
     int image_height = int(image_width/aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
@@ -61,10 +77,4 @@ int main() {
     }
 
     std::clog << "\rDone.               \n";
-    std::clog << "Image width: " << image_width << " Image height: " << image_height << "\n";
-    std::clog << "Camera center: " << camera_center << "\n";
-    std::clog << "Pixel00 location: " << pixel00_loc << "\n"; 
-    std::clog << "Pixel00 unit vector: " << unit_vector(pixel00_loc) << "\n";
-    std::clog << "Pixel_du: " << pixel_du << " Pixel_dv: " << pixel_dv << "\n";
-    std::clog << "Max unit y: " << mx << " Min unit y: " << mn << "\n";
 }
