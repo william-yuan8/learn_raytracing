b078b3de70380f0c926930cf898955f55d72268a
William <williamyuan2016@gmail.com>
2026-02-15 12:20:00 -0500

    added gamma correction (transform from linear space to gamma space)

--- color.h
+++ color.h
@@ -6,12 +6,18 @@
 
 using color = vec3;
 
-void write_color(std::ostream& out, const color& pix_col) {
-    static const interval color_interval(0.0, 0.999);
+inline double to_gamma(double x) {
+    if (x > 0)  return std::sqrt(x);
 
-    int r = int(color_interval.clamp(pix_col[0])*255.999);
-    int g = int(color_interval.clamp(pix_col[1])*255.999);
-    int b = int(color_interval.clamp(pix_col[2])*255.999);
+    return 0;
+}
+
+void write_color(std::ostream& out, const color& pix_col) {
+    static const interval intensity(0, 0.999);
+
+    int r = int(intensity.clamp(to_gamma(pix_col.x()))*256);
+    int g = int(intensity.clamp(to_gamma(pix_col.y()))*256);
+    int b = int(intensity.clamp(to_gamma(pix_col.z()))*256);
 
     out << r << " " << g << " " << b << "\n";
 }
